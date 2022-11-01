0b142aad6dfaa9f1cda31881045d6461186c7c50
Beto Toro <beto.toro@zemoga.com>
2022-11-01 11:47:27 -0500

    Initial Commit

---
+++ RollDice/RollDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollDiceApp.swift
+//  RollDice
+//
+//  Created by Beto Toro on 1/11/22.
+//
+
+import SwiftUI
+
+@main
+struct RollDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
