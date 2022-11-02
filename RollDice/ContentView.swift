aa28cbbb96a6db2e12a3507f1488beaa24f5d711
Beto Toro <betotoro0902@gmail.com>
2022-11-02 11:02:14 -0500

    [feat] basic funcionality app

--- RollDice/ContentView.swift
+++ RollDice/ContentView.swift
@@ -9,12 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
   let diceTypes = [4, 6, 8, 10, 12, 20, 100]
+  let columns: [GridItem] = [
+    .init(.adaptive(minimum: 60))
+  ]
   
   @AppStorage("selectedDiceType") var selectedDiceType = 6
   @AppStorage("numberToRoll") var numberToRoll = 4
   
+  @State private var currentResult = DiceResult(type: 0, number: 0)
+  
   func rollDice() {
-    
+    currentResult = DiceResult(type: selectedDiceType, number: numberToRoll)
   }
   
   var body: some View {
@@ -28,9 +33,23 @@ struct ContentView: View {
           }
           .pickerStyle(.segmented)
           
-          Stepper("Number To Roll", value: $numberToRoll, in: 1...20)
+          Stepper("Number of dice: \(numberToRoll)", value: $numberToRoll, in: 1...20)
           
           Button("Roll Them!", action: rollDice)
+        } footer: {
+          LazyVGrid(columns: columns) {
+            ForEach(0..<currentResult.rolls.count, id: \.self) { rollNumer in
+              Text("\(currentResult.rolls[rollNumer])")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .aspectRatio(1, contentMode: .fit)
+                .foregroundColor(.black)
+                .background(.white)
+                .cornerRadius(10)
+                .shadow(radius: 3)
+                .font(.title)
+                .padding(5)
+            }
+          }
         }
       }
       .navigationTitle("High Rollers")
