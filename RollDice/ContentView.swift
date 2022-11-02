56a51d8a1981b9efda831c6231ef457e89a3c156
Beto Toro <betotoro0902@gmail.com>
2022-11-02 14:57:17 -0500

    [feat] complete the app

--- RollDice/ContentView.swift
+++ RollDice/ContentView.swift
@@ -12,14 +12,51 @@ struct ContentView: View {
   let columns: [GridItem] = [
     .init(.adaptive(minimum: 60))
   ]
+  let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+  let savedPaths = FileManager.documentsDirectory.appendingPathComponent("SavedRolls.json")
+  @State private var savedResults = [DiceResult]()
   
   @AppStorage("selectedDiceType") var selectedDiceType = 6
   @AppStorage("numberToRoll") var numberToRoll = 4
   
   @State private var currentResult = DiceResult(type: 0, number: 0)
+  @State private var stoppedDice = 0
+  @State private var feedback = UIImpactFeedbackGenerator(style: .rigid)
+  
+  func load() {
+    if let data = try? Data(contentsOf: savedPaths) {
+      if let results = try? JSONDecoder().decode([DiceResult].self, from: data) {
+        savedResults = results
+      }
+    }
+  }
+  
+  func save() {
+    if let data = try? JSONEncoder().encode(savedResults) {
+      try? data.write(to: savedPaths, options: [.atomic, .completeFileProtection])
+    }
+  }
   
   func rollDice() {
     currentResult = DiceResult(type: selectedDiceType, number: numberToRoll)
+    stoppedDice = -8
+  }
+  
+  func updateDice() {
+    guard stoppedDice < currentResult.rolls.count else { return }
+    
+    for i in stoppedDice..<numberToRoll {
+      if i < 0 { continue }
+      currentResult.rolls[i] = Int.random(in: 1...selectedDiceType)
+      feedback.impactOccurred()
+    }
+    
+    stoppedDice += 1
+    
+    if stoppedDice == numberToRoll {
+      savedResults.insert(currentResult, at: 0)
+      save()
+    }
   }
   
   var body: some View {
@@ -51,8 +88,25 @@ struct ContentView: View {
             }
           }
         }
+        .disabled(stoppedDice < currentResult.rolls.count)
+        
+        if savedResults.isEmpty == false {
+          Section("Previous results") {
+            ForEach(savedResults) { result in
+              VStack(alignment: .leading) {
+                Text("\(result.number) x D\(result.type)")
+                  .font(.headline)
+                Text(result.rolls.map(String.init).joined(separator: ", "))
+              }
+            }
+          }
+        }
       }
       .navigationTitle("High Rollers")
+      .onReceive(timer) { date in
+        updateDice()
+      }
+      .onAppear(perform: load)
     }
   }
 }
